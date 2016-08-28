f54c3daaa2bfe436f535a8fd2e9c96e6ae177c51
alexisHetic <alexisHetic>
2016-08-28 09:53:32 +0200

    Add Temporary files creation

--- app/lib/downloadImage.js
+++ app/lib/downloadImage.js
@@ -1,15 +1,12 @@
 const fs = require('fs')
 const request = require('request')
+const temp = require('temp')
 
 const downloadImage = (image) => {
-  const dir = './downloads/'
-  const path = dir + image.id + '.jpg';
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+  const path = temp.path({ suffix: '.jpg' })
   return new Promise((resolve, reject) => {
     request(image.uri, (err, res, body) => {
       if (err) return reject(err);
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
       return request(image.uri)
         .pipe(fs.createWriteStream(path))
         .on('close', resolve.bind(null, path));

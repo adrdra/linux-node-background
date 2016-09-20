5912b518262f2dc54e83b5508249731fc548b879
alexisHetic <alexisHetic>
2016-09-20 20:58:18 +0200

    Update gitignore

---
+++ config/unsplash.js
@@ -0,0 +1,4 @@
+module.exports = {
+  applicationId: '8d85aea5476c0c0b8f98b84b63c2e420e77755f6f194c6c8bbe85294ee6aa248',
+  secret: '1e3740888bdb049370de88f04941915af9e79331f13fcbaf74fc5b6cc450d3bc',
+}
